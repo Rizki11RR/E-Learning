1b507a6325c8a1c0afc9ccd2bc5e204b0a9e7891
Rizki11RR <213200203@almaata.ac.id>
2022-11-06 14:51:19 +0700

    frontend

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-
+//tes
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
